1daee36d0e2a354f4ae55547f8750776e716fabf
Yan Moroz <mail@yanmoroz.ru>
2024-02-12 17:24:27 +0300

    Initial Commit

---
+++ ScrumMeetings/ScrumMeetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumMeetingsApp.swift
+//  ScrumMeetings
+//
+//  Created by Yan Moroz on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumMeetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
